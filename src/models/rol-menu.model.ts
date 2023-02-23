aa6965afc8a8ced043a0b38f91530244a597b56d
LinaNieto <lina.nieto22243@ucaldas.edu.co>
2023-02-23 14:45:03 -0500

    Creacion de modelos de microservicio de seguridad

---
+++ src/models/rol-menu.model.ts
@@ -0,0 +1,57 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class RolMenu extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  listar: boolean;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  guardar: boolean;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  editar: boolean;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  eliminar: boolean;
+
+  @property({
+    type: 'boolean',
+    required: true,
+  })
+  descragar: boolean;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<RolMenu>) {
+    super(data);
+  }
+}
+
+export interface RolMenuRelations {
+  // describe navigational properties here
+}
+
+export type RolMenuWithRelations = RolMenu & RolMenuRelations;
---
+++ src/models/rol.model.ts
@@ -0,0 +1,38 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({settings: {strict: false}})
+export class Rol extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  _id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  nombre: string;
+
+  @property({
+    type: 'string',
+  })
+  Comentarios?: string;
+
+  // Define well-known properties here
+
+  // Indexer property to allow additional data
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [prop: string]: any;
+
+  constructor(data?: Partial<Rol>) {
+    super(data);
+  }
+}
+
+export interface RolRelations {
+  // describe navigational properties here
+}
+
+export type RolWithRelations = Rol & RolRelations;
